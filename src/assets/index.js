3f8c0fd0a2f2ac03a46ad9f23853b1006adeb94c
grashibam <86063289+grashibam@users.noreply.github.com>
2023-05-22 19:15:32 +0530

    up

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,10 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import aqb from "./aqb.png";
+import bdw from "./bdw.png";
+import bug from "./bug.png";
+
 
 export {
   logo,
@@ -59,4 +63,7 @@ export {
   carrent,
   jobit,
   tripguide,
+  aqb,
+  bdw,
+  bug,
 };

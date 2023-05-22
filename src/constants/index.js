dee21546986ad7687a26d8ac9de50752f86d7263
grashibam <86063289+grashibam@users.noreply.github.com>
2023-05-22 19:22:48 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -193,7 +193,7 @@ import {
     {
       name: "Blood Donation Website",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "A management platform where the donors can donate and recieve bloods in case of emergency",
       tags: [
         {
           name: "PHP",
@@ -214,18 +214,18 @@ import {
     {
       name: "Bug Detection",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "Detects bugs present in crops",
       tags: [
         {
-          name: "react",
+          name: "Data Science",
           color: "blue-text-gradient",
         },
         {
-          name: "restapi",
+          name: "Tensorflow",
           color: "green-text-gradient",
         },
         {
-          name: "scss",
+          name: "Cascade-Classifier",
           color: "pink-text-gradient",
         },
       ],
@@ -235,7 +235,7 @@ import {
     {
       name: "Any question answer website",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "A look alike ChatGPT which approximately answers all questions.",
       tags: [
         {
           name: "nextjs",
